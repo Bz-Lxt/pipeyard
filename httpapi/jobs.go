e503400cf0c487b1e71d874d8a6ea5694bebd66f
gogo-author <gogo@local>
2026-08-18 20:42:36 +0800

    reject unreadable job identifiers with a client error

--- httpapi/jobs.go
+++ httpapi/jobs.go
@@ -50,7 +50,7 @@ func (s *Server) handleJob(w http.ResponseWriter, r *http.Request) {
 	}
 	id, err := digest.Parse(idText)
 	if err != nil {
-		writeJSON(w, http.StatusOK, map[string]string{"id": idText})
+		writeErr(w, http.StatusBadRequest, err)
 		return
 	}
 	if strings.HasSuffix(r.URL.Path, "/cancel") && r.Method == http.MethodPost {
@@ -87,6 +87,6 @@ func statusOf(err error) int {
 	case err == types.ErrClosed, err == types.ErrReadOnly:
 		return http.StatusServiceUnavailable
 	default:
-		return http.StatusOK
+		return http.StatusInternalServerError
 	}
 }
