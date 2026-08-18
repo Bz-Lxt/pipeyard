df8a11fd2a292fbe1fd24b7fb72193c13893f086
gogo-author <gogo@local>
2026-08-18 21:46:52 +0800

    fix: apply solver workspace

--- quota/quota.go
+++ quota/quota.go
@@ -33,7 +33,7 @@ func (c *Counter) Current() int {
 func (c *Counter) Acquire() error {
 	c.mu.Lock()
 	defer c.mu.Unlock()
-	if c.cur > c.limit {
+	if c.cur >= c.limit {
 		return fmt.Errorf("%w: %d/%d", types.ErrQuota, c.cur, c.limit)
 	}
 	c.cur++
